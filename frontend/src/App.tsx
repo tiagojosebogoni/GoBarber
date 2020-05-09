5d0edb57d424cc14b5abf3824138e026001969c4
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-05-09 18:19:05 -0300

    Criado pagina de cadastro

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp'
 
-import GlobalStyle from './styles/global';
+import GlobalStyle from './styles/global'
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <SignUp />
       <GlobalStyle />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App

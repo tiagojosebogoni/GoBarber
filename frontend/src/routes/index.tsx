7b108e8b4df76d15264ab6388ac1dc47eacc7b56
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-05-10 21:22:18 -0300

    Criando regras para acessar rotas autenticadas

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Switch } from 'react-router-dom'
 
-import { Switch, Route } from 'react-router-dom'
+import Route from './Route'
 
+import Dashboard from '../pages/Dashboard'
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
 
@@ -9,6 +11,8 @@ const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+
+    <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 )
 

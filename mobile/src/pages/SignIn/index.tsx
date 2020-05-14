1dba1d42e51b6f11761821f54ff4422e59779c9d
Tiago Bogoni <tiagojosebogoni@gmail.com>
2020-05-13 22:25:33 -0300

    Terminando tela de SignIn e SignUp

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,17 +1,75 @@
 import React from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native'
 
-import { Container, Title } from './styles'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles'
 
 import logoImg from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
+  const navigation = useNavigation()
   return (
-    <Container>
-      <Image source={logoImg} />
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Title>Faça seu logon</Title>
-    </Container>
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                console.log('agora')
+              }}
+            >
+              Entrar
+            </Button>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+            <Title>Faça seu logon</Title>
+          </Container>
+        </ScrollView>
+        <CreateAccountButton
+          onPress={() => {
+            navigation.navigate('SignUp')
+          }}
+        >
+          <Icon name="log-in" size={20} color="#ff9000" />
+          <CreateAccountButtonText>Criar conta</CreateAccountButtonText>
+        </CreateAccountButton>
+      </KeyboardAvoidingView>
+    </>
   )
 }
 
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,9 +1,65 @@
 import React from 'react'
+import {
+  Image,
+  View,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Feather'
 
-import { Container } from './styles'
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+
+import logoImg from '../../assets/logo.png'
 
 const SignUp: React.FC = () => {
-  return <Container />
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        enabled
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+
+            <Button
+              onPress={() => {
+                navigation.navigate('SignIn')
+              }}
+            >
+              Entrar
+            </Button>
+          </Container>
+        </ScrollView>
+        <BackToSignIn
+          onPress={() => {
+            navigation.goBack()
+          }}
+        >
+          <Icon name="arrow-left" size={20} color="#fff" />
+          <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+      </KeyboardAvoidingView>
+    </>
+  )
 }
 
 export default SignUp
